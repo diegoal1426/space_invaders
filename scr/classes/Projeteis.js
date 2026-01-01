adfe607b307d8d100683313d49799c8470f30ead
Diego Ferrari <44404382+diegoal1426@users.noreply.github.com>
2026-01-01 20:49:20 -0300

    Create Projeteis.js

---
+++ scr/classes/Projeteis.js
@@ -0,0 +1,18 @@
+export class Projeteis {
+    constructor(position, velocidade){
+        this.position = position;
+        this.width = 2;
+        this.height = 20;
+        this.velocidade = velocidade;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
+        this.update();
+    }
+
+    update(){
+        this.position.y += this.velocidade
+    }
+}
