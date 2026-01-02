7f8213030a853d8ef6c226b4555582c3d8ad286e
Diego Ferrari <44404382+diegoal1426@users.noreply.github.com>
2026-01-01 21:20:29 -0300

    Add files via upload
    Signed-off-by: Diego Ferrari <44404382+diegoal1426@users.noreply.github.com>

---
+++ src/utils/constantes.js
@@ -0,0 +1,19 @@
+export const CAMINHO_ESPACONAVE       = "src/assets/images/spaceship.png";
+export const CAMINHO_MOTOR_ESPACONAVE = "src/assets/images/engine.png";
+export const CAMINHO_FOGO_MOTOR       = "src/assets/images/engine_sprites.png";
+export const CAMINHO_INVASOR          = "src/assets/images/invader.png";
+
+
+export const CAMINHO_SOM_EXPLOSAO     = "src/assets/audios/explosion.mp3";
+export const CAMINHO_SOM_HIT          = "src/assets/audios/hit.mp3";
+export const CAMINHO_SOM_NOVO_NIVEL   = "src/assets/audios/next_level.mp3";
+export const CAMINHO_SOM_TIROS        = "src/assets/audios/shoot.mp3";
+
+export const FRAMES_INICIAL           = 8;
+export const ESCALA_PLAYER            = 2;
+
+export const GAMESTATE                = {
+                                            START: "start",
+                                            PLAYING: "playing",
+                                            GAMEOVER: "gameover"
+                                        };
